dc549702a2d4914443e18b47e5a44dd4f487296e
James Lawson <jameslawson@users.noreply.github.com>
2016-10-01 16:22:31 +0100

    Cleanup

---
+++ src/paragraph/rule/rle.js
@@ -0,0 +1,40 @@
+import { RLE, MAX_DEPTH } from '../../type';
+import { DirectionalStatusStackEntry } from '../../type';
+import flow from 'lodash.flow';
+
+// http://unicode.org/reports/tr9/#X2
+// [1]: "Compute the least odd embedding level greater than the embedding level of
+//       the last entry on the directional status stack"
+// [2]: at max_depth or if either overflow count is non-zero, the level remains the same (overflow RLE).
+function rle(ch, index, state) {
+  if (ch !== RLE) return state;
+
+  const lastLevel = state.get('directionalStatusStack').peek().get('level');
+  return flow(
+    function(state) {
+      return state.setIn(['embeddingLevels', 'levels', index], lastLevel);
+    },
+    function(state) {
+      const newLevel = (lastLevel + 1) + (lastLevel % 2);
+      const newLevelInvalid = (newLevel >= MAX_DEPTH); // [2]
+
+      const isolate = state.get('overflowIsolateCount');
+      const embedding = state.get('overflowEmbeddingCount');
+      const isCurrentOverflow = (isolate > 0 || embedding > 0); // [2]
+      const isOverflowRLE = (newLevelInvalid || isCurrentOverflow); // [2]
+
+      if (isOverflowRLE) {
+        if (isolate === 0) {
+          return state.update('overflowEmbeddingCount', increase);
+        } else {
+          return state;
+        }
+      } else {
+        const newEntry = new DirectionalStatusStackEntry({ level: newLevel });
+        return state.update('directionalStatusStack', (stack) => stack.push(newEntry));
+      }
+    }
+  )(state);
+}
+
+export { rle };
---
+++ src/paragraph/rule/rules.js
@@ -0,0 +1,24 @@
+import { rle } from './rle';
+import lre from './lre';
+import rlo from './rlo';
+function lro(ch, index, state) { return state; }
+import { rli } from './rli';
+import { lri } from './lri';
+function fsi(ch, index, state) { return state; }
+import other from './other';
+import { pdi } from './pdi';
+import { pdf } from './pdf';
+
+export {
+  rle,   // X2.
+  lre,   // X3.
+  //rlo  // X4.
+  //lro  // X5.
+  rli,   // X5a.
+  lri,   // X5b.
+  //fsi  // X5c.
+  other, // X6.
+  pdi,   // X6a.
+  pdf    // X7.
+}
+
