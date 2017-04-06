7278e081a01df1006148631db798daa932980ffd
James Lawson <jameslawson@users.noreply.github.com>
2017-04-06 10:34:40 +0100

    Add API

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { resolvedLevelsWithInvisibles } from './resolve/resolvedLevels';
+import { reorderedLevels } from './resolve/reorderedLevels';
+
+// Public API
+export {
+  resolvedLevelsWithInvisibles as resolve,
+  reorderedLevels as reorder
+};
