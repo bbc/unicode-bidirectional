d6a6313110b947e01dc704acf0b949233f925a2f
James Lawson <jameslawson@users.noreply.github.com>
2017-06-29 16:01:41 +0100

    Update example1.js

--- example/example1.js
+++ example/example1.js
@@ -1,6 +1,8 @@
-// Node.js
-// Browser: Webpack, browserify
-import { resolve, reorder } from 'unicode-bidirectional/es6';
+// Example 1. ES6
+// Targeting Node requires a transpiler e.g. babel, buble, traceur, ...
+// Targeting Browser requires a transpiler and a bundler e.g. Webpack, browserify, ...
+
+import { resolve, reorder } from 'unicode-bidirectional';
 
 const codepoints = [0x28, 0x29, 0x2A, 0x05D0, 0x05D1, 0x05D2]
 const levels = resolve(codepoints, 0);  // [0, 0, 0, 1, 1, 1]
