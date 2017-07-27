8af55be8a8633046d483baffe656116909e076a5
James Lawson <jameslawson@users.noreply.github.com>
2017-07-27 13:11:13 +0100

    Add Map to fillWithCodepoints
    - add AL case (see issue #5)

--- test/conform/fillWithCodepoints.js
+++ test/conform/fillWithCodepoints.js
@@ -5,39 +5,40 @@ import {
   L1 as L,
   R1 as R,
   ON1 as ON,
+  AL1 as AL,
   AN1 as AN,
   EN1 as EN,
   WS1 as WS,
   BN1 as BN
 } from '../../src/util/constant';
 
+const fillMap = new Map([
+  ['ALM', ALM],
+  ['AL', AL],
+  ['AN', AN],
+  ['B', B],
+  ['BN', BN],
+  ['EN', EN],
+  ['FSI', FSI],
+  ['L', L],
+  ['LRE', LRE],
+  ['LRI', LRI],
+  ['LRM', LRM],
+  ['LRO', LRO],
+  ['ON', ON],
+  ['PDF', PDF],
+  ['PDI', PDI],
+  ['R', R],
+  ['S', S],
+  ['RLE', RLE],
+  ['RLI', RLI],
+  ['RLM', RLM],
+  ['RLO', RLO],
+  ['WS', WS]
+]);
+
 function fillWithCodepoints(bidiTypes) {
-  return bidiTypes.map(t => {
-
-    if (t === 'ALM') return ALM;
-    if (t === 'AN') return AN;
-    if (t === 'B') return B;
-    if (t === 'BN') return BN;
-    if (t === 'EN') return EN;
-    if (t === 'FSI') return FSI;
-    if (t === 'L') return L;
-    if (t === 'LRE') return LRE;
-    if (t === 'LRI') return LRI;
-    if (t === 'LRM') return LRM;
-    if (t === 'LRO') return LRO;
-    if (t === 'ON') return ON;
-    if (t === 'PDF') return PDF;
-    if (t === 'PDI') return PDI;
-    if (t === 'R') return R;
-    if (t === 'S') return S;
-    if (t === 'RLE') return RLE;
-    if (t === 'RLI') return RLI;
-    if (t === 'RLM') return RLM;
-    if (t === 'RLO') return RLO;
-    if (t === 'WS') return WS;
-
-    return t;
-  });
+  return bidiTypes.map(t => fillMap.get(t));
 }
 
 export default fillWithCodepoints;
